bf3de24e2021696d38a194472f436627aa37c6f3
Codename O'Connell <oconnellcoder@gmail.com>
2023-02-28 18:07:17 -0500

    fixed add a role, fixed add an employee, added update an employee role, and added delete a record

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const init = () => {
         if(task == 'add a department') DB.addDepartment(ans).then(init);
         if(task == 'add a role') DB.addRole(ans).then(init);
         if(task == 'add an employee') DB.addEmployee(ans).then(init);
+        if(task == 'update an employee role') DB.updateEmployee(ans).then(init);
+        if(task == 'delete a record') DB.removeRecord(ans).then(init);
     });
 };
 
